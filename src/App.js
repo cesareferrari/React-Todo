817f372f7428624fc772f5cd62c147188bd042c9
Cesare Ferrari <cesare@maryandferrari.com>
2019-05-14 22:04:06 -0400

    Add state to App

--- src/App.js
+++ src/App.js
@@ -16,14 +16,17 @@ const todos = [
 ];
 
 class App extends React.Component {
-  // you will need a place to store your state in this component.
-  // design `App` to be the parent component of your application.
-  // this component is going to take care of state, and any change handlers you need to work with your state
+  constructor() {
+    super();
+    this.state = {
+      todos: todos
+    }
+  }
 
   render() {
     return (
       <div>
-        <TodoList todos={todos} />
+        <TodoList todos={this.state.todos} />
         <TodoForm />
       </div>
     );
